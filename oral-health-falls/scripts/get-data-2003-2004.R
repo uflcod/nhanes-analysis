dc494b629c1c46d863c0d35089eb169730949dc6
Bill Duncan <wdduncan@users.noreply.github.com>
2025-02-08 14:03:14 -0500

    remove data tsvs

--- oral-health-falls/scripts/get-data-2003-2004.R
+++ oral-health-falls/scripts/get-data-2003-2004.R
@@ -11,7 +11,7 @@ write_data <- function(table, file_name) {
 demo_c <- nhanes('DEMO_C')
 write_data(demo_c, 'DEMO_C_demographics.tsv')
 
-# food security data
+# food security questionnaire data
 fsq_c <- nhanes('FSQ_C')
 write_data(fsq_c, 'FSQ_C_food-security_questionnaire.tsv')
 
@@ -21,15 +21,15 @@ write_data(diq_c, 'DIQ_C_diabetes_questionnaire.tsv')
 
 # tobacco use questionnaire data
 smq_c <- nhanes('SMQ_C')
-write_data(smq_c, 'SMQ_C_tobacco_use_questionnaire.tsv')
+write_data(smq_c, 'SMQ_C_tobacco-use_questionnaire.tsv')
 
 # medical conditions questionnaire
 mcq_c <- nhanes('MCQ_C')
-write_data(mcq_c, 'MCQ_C_medical_conditions_questionnaire.tsv')
+write_data(mcq_c, 'MCQ_C_medical-conditions_questionnaire.tsv')
 
 # Rx questionnaire data
 rx_c <- nhanes('RXQ_RX_C')
-write_data(rx_c, 'RXQ_RX_C_rx_medications_questionnaire.tsv')
+write_data(rx_c, 'RXQ_RX_C_rx-medications_questionnaire.tsv')
 
 # balance exam data
 bax_c <- nhanes('BAX_C')
@@ -41,7 +41,7 @@ write_data(baq_c, 'BAX_C_balance_questionnaire.tsv')
 
 # oral health questionnaire data
 ohq_c <- nhanes('OHQ_C')
-write_data(ohq_c, 'OHQ_C_oral_health_questionnaire.tsv')
+write_data(ohq_c, 'OHQ_C_oral-health_questionnaire.tsv')
 
 # dentition exam data
 dent_c <- nhanes('OHXDEN_C')
